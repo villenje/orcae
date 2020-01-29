72ac27013f23b14893959247f95dab08e5a958e1
villenje <jerome.villeneuve@gipsa-lab.fr>
2020-01-29 11:05:03 +0100

    up
    petit "plus" pour les contenus additionnels relatifs aux songs et debug timmer

--- js/main.js
+++ js/main.js
@@ -38,5 +38,28 @@ $('.button-group').each( function( i, buttonGroup ) {
 });
 
 
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.maxHeight){
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+            $grid.isotope();
+
+        });
+
+    }
+
 });
+
+
+
+
 
